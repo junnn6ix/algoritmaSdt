b890b4b6305eec3100448023519dda8a7e0f44fa
neckjs <98792546+junnn6ix@users.noreply.github.com>
2024-09-30 09:50:07 +0700

    ASKDjhAKDasd

---
+++ pertemuan3.py
@@ -0,0 +1,10 @@
+print("Penerapan record\n")
+
+mahasiswa = {
+    "nama": "Usep gerengesng",
+    "usia": 199,
+    "kelas": "IF 15 D",
+    "IPS": 4.90
+}
+
+print("Nama: ", mahasiswa["nama"], "\nKelas: ", mahasiswa["usia"], "\nIPS: ", mahasiswa["IPS"])
