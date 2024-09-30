0b91e2360eca3ff41e6786c7c53f85c68186c769
neckjs <98792546+junnn6ix@users.noreply.github.com>
2024-09-30 10:21:15 +0700

    kjsafafsdba

--- pertemuan3.py
+++ pertemuan3.py
@@ -7,4 +7,11 @@ mahasiswa = {
     "IPS": 4.90
 }
 
-print("Nama: ", mahasiswa["nama"], "\nKelas: ", mahasiswa["usia"], "\nIPS: ", mahasiswa["IPS"])
+
+print("Nama: ", mahasiswa["nama"], "\nKelas: ", mahasiswa["usia"], "\nIPS: ", mahasiswa["IPS"], "\nUsia: ", mahasiswa["usia"])
+
+mahasiswa['usia'] = 877
+
+print("Usia setelah ulang tahun ke-x: ", mahasiswa["usia"])
+
+print("anjais")
